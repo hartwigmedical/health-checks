54d9e82783e0bfa6b63c714c1112624306741eaa
Wilder Rodrigues <wilder.rodrigues@ekholabs.nl>
2016-06-02 18:33:10 +0200

    Add unit tests to CheckType and HealthChecksFlyweight.

---
+++ src/test/java/com/hartwig/healthchecks/util/adapter/HealthChecksFlyweightTest.java
@@ -0,0 +1,38 @@
+package com.hartwig.healthchecks.util.adapter;
+
+import com.hartwig.healthchecks.boggs.adapter.BoggsAdapter;
+import com.hartwig.healthchecks.common.adapter.HealthCheckAdapter;
+import com.hartwig.healthchecks.common.exception.NotFoundException;
+import mockit.Expectations;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HealthChecksFlyweightTest {
+
+  @Test
+  public void getAdapterSuccess() {
+    HealthChecksFlyweight healthChecksFlyweight = HealthChecksFlyweight.getInstance();
+
+    Assert.assertNotNull(healthChecksFlyweight);
+
+    try {
+      HealthCheckAdapter boggsAdapter = healthChecksFlyweight.getAdapter("boggs");
+
+      Assert.assertTrue(boggsAdapter instanceof BoggsAdapter);
+    } catch (NotFoundException e) {
+      Assert.fail();
+    }
+  }
+
+  @Test(expected = NotFoundException.class)
+  public void getAdapterFailure() throws NotFoundException {
+    HealthChecksFlyweight healthChecksFlyweight = HealthChecksFlyweight.getInstance();
+
+    Assert.assertNotNull(healthChecksFlyweight);
+
+    new Expectations() {{
+      healthChecksFlyweight.getAdapter("bugs");
+      result = new NotFoundException("Expected error.");
+    }};
+  }
+}
