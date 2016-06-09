f120d9abf7c427eb3ef0f67036a0ab42953f94ad
Noha Hazaa <nhazaa@schubergphilis.com>
2016-06-09 13:34:59 +0200

    Change prestats to include all data not only failed

--- boggs/src/main/java/com/hartwig/healthchecks/boggs/healthcheck/prestast/PrestatsExtractor.java
+++ boggs/src/main/java/com/hartwig/healthchecks/boggs/healthcheck/prestast/PrestatsExtractor.java
@@ -1,9 +1,7 @@
 package com.hartwig.healthchecks.boggs.healthcheck.prestast;
 
-import com.hartwig.healthchecks.boggs.model.report.PrestatsReport;
-import com.hartwig.healthchecks.common.util.CheckType;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import static java.util.stream.Collectors.toCollection;
+import static java.util.stream.Collectors.toList;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,46 +12,44 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.toCollection;
-import static java.util.stream.Collectors.toList;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.hartwig.healthchecks.boggs.model.report.PrestatsDataReport;
+import com.hartwig.healthchecks.boggs.model.report.PrestatsReport;
+import com.hartwig.healthchecks.common.util.CheckType;
 
 public class PrestatsExtractor {
 
 	private static Logger LOGGER = LogManager.getLogger(PrestatsExtractor.class);
 
 	private static final String SEPERATOR_REGEX = "\t";
-	private static final String FAIL_ERROR = "FAIL";
 	private static String DATA_FILE_NAME = "summary.txt";
 
 	public PrestatsReport extractFromRunDirectory(String runDirectory) throws IOException {
 		List<Path> summaryFiles = Files.walk(new File(runDirectory).toPath())
-				.filter(p -> p.getFileName().toString().startsWith(DATA_FILE_NAME))
-				.sorted()
+				.filter(p -> p.getFileName().toString().startsWith(DATA_FILE_NAME)).sorted()
 				.collect(toCollection(ArrayList<Path>::new));
 
 		PrestatsReport prestatsData = new PrestatsReport(CheckType.PRESTATS);
 
-		summaryFiles.stream()
-				.map(path -> {
-					Stream<String> fileLines = Stream.empty();
-					try {
-						fileLines =  Files.lines(path);
-					} catch (IOException e) {
-						LOGGER.error(String.format("Error occurred when reading file. Will return empty stream. Error -> %s", e.getMessage()));
-					}
-					return fileLines.collect(toList());
-				})
-				.flatMap(Collection::stream)
-				.filter(line -> line.contains(FAIL_ERROR))
-				.map(line -> {
-					String check = line.split(SEPERATOR_REGEX)[1];
-					String file = line.split(SEPERATOR_REGEX)[2];
-					return new String[]{check, file};
-				})
-				.forEach(checkLine -> {
-                    prestatsData.addData(checkLine[1], checkLine[0]);
-				});
-
+		summaryFiles.stream().map(path -> {
+			Stream<String> fileLines = Stream.empty();
+			try {
+				fileLines = Files.lines(path);
+			} catch (IOException e) {
+				LOGGER.error(String.format("Error occurred when reading file. Will return empty stream. Error -> %s",
+						e.getMessage()));
+			}
+			return fileLines.collect(toList());
+		}).flatMap(Collection::stream).map(line -> {
+			String status = line.split(SEPERATOR_REGEX)[0];
+			String check = line.split(SEPERATOR_REGEX)[1];
+			String file = line.split(SEPERATOR_REGEX)[2];
+			return new PrestatsDataReport(status, check, file);
+		}).forEach(prestatsDataReport -> {
+			prestatsData.addData(prestatsDataReport);
+		});
 		return prestatsData;
 	}
 }
--- boggs/src/test/java/com/hartwig/healthchecks/boggs/io/PrestatsExtractorTest.java
+++ boggs/src/test/java/com/hartwig/healthchecks/boggs/io/PrestatsExtractorTest.java
@@ -22,7 +22,7 @@ public class PrestatsExtractorTest {
 		PrestatsReport prestatsData = extractor.extractFromRunDirectory(runDirURL.getPath().toString());
 
 		assertNotNull("We should get some fails", prestatsData);
-		assertEquals("Number of files that has failed is not correct", 2,  prestatsData.getSummary().size());
+		assertEquals("Number of files that has failed is not correct", 24,  prestatsData.getSummary().size());
 
 	}
 
