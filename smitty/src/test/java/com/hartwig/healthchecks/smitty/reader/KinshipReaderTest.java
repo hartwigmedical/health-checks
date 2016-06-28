a0e92f4f4adcef4502ffbe61bd492086ff7d36a4
Noha Hazaa <nhazaa@schubergphilis.com>
2016-06-28 10:45:16 +0200

    Adding Code for kinship file reading;  - Adding file reader and test  - Adding a new module

---
+++ smitty/src/test/java/com/hartwig/healthchecks/smitty/reader/KinshipReaderTest.java
@@ -0,0 +1,59 @@
+package com.hartwig.healthchecks.smitty.reader;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.NoSuchFileException;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.google.common.io.Resources;
+
+public class KinshipReaderTest {
+
+    private static final String WRONG_NUM_LINES = "Wrong # of Lines";
+
+    private static final String NOT_NULL = "Should Not Be null";
+
+    private static final String TEST_DIR = "rundir";
+
+    private static final String EMPTY_DIR = "emptyFiles";
+
+    private static final String NO_FILE_DIR = "empty";
+
+    @Test
+    public void readKinship() throws IOException {
+        final URL exampleFlagStatURL = Resources.getResource(TEST_DIR);
+        final KinshipReader kinshipReader = new KinshipReader();
+        final List<String> readLines = kinshipReader.readLinesFromKinship(exampleFlagStatURL.getPath());
+        assertNotNull(NOT_NULL, readLines);
+        final int EXPECTED_NUM_LINES = 4;
+        assertEquals(WRONG_NUM_LINES, EXPECTED_NUM_LINES, readLines.size());
+    }
+
+    @Test
+    public void readEmptyKinship() throws IOException {
+        final URL exampleFlagStatURL = Resources.getResource(EMPTY_DIR);
+        final KinshipReader kinshipReader = new KinshipReader();
+        final List<String> readLines = kinshipReader.readLinesFromKinship(exampleFlagStatURL.getPath());
+        assertNotNull(NOT_NULL, readLines);
+        assertEquals(NOT_NULL, 0, readLines.size());
+    }
+
+    @Test(expected = FileNotFoundException.class)
+    public void readNoKinship() throws IOException {
+        final URL exampleFlagStatURL = Resources.getResource(NO_FILE_DIR);
+        final KinshipReader kinshipReader = new KinshipReader();
+        kinshipReader.readLinesFromKinship(exampleFlagStatURL.getPath());
+    }
+
+    @Test(expected = NoSuchFileException.class)
+    public void readNoneExistingFolder() throws IOException {
+        final KinshipReader kinshipReader = new KinshipReader();
+        kinshipReader.readLinesFromKinship("bla");
+    }
+}
