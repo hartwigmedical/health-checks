4967964425464566067d8f77fa16e69fa7c17398
Noha Hazaa <nhazaa@schubergphilis.com>
2016-06-29 18:48:37 +0200

    Fix the kinship to be reading one line file

--- smitty/src/main/java/com/hartwig/healthchecks/smitty/report/KinshipReport.java
+++ smitty/src/main/java/com/hartwig/healthchecks/smitty/report/KinshipReport.java
@@ -1,7 +1,5 @@
 package com.hartwig.healthchecks.smitty.report;
 
-import java.util.List;
-
 import org.jetbrains.annotations.NotNull;
 
 import com.hartwig.healthchecks.common.report.BaseDataReport;
@@ -12,15 +10,15 @@ public class KinshipReport extends BaseReport {
 
     private static final long serialVersionUID = -5744830259786248569L;
 
-    private final List<BaseDataReport> patientData;
+    private final BaseDataReport patientData;
 
-    public KinshipReport(final CheckType checkType, final List<BaseDataReport> knishipData) {
+    public KinshipReport(final CheckType checkType, final BaseDataReport knishipData) {
         super(checkType);
         patientData = knishipData;
     }
 
     @NotNull
-    public List<BaseDataReport> getPatientData() {
+    public BaseDataReport getPatientData() {
         return patientData;
     }
 
