9be582ead6edb1d8dddad448bbe0515443a48931
Noha Hazaa <nhazaa@schubergphilis.com>
2016-07-15 15:44:53 +0200

    Fix Codacy issue

--- nesbit/src/main/java/com/hartwig/healthchecks/nesbit/extractor/SomaticExtractor.java
+++ nesbit/src/main/java/com/hartwig/healthchecks/nesbit/extractor/SomaticExtractor.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -117,7 +118,8 @@ public class SomaticExtractor extends AbstractVCFExtractor {
         final List<VCFSomaticSetData> callerSetsPerCallersCount = getSetForCallerWithMoreThanOneCaller(callerSets,
                         caller);
         final double precision = (double) callerSetsPerCallersCount.size() / callerSets.size();
-        return new BaseDataReport(patientId, String.format(PRECISION_CHECK_LABEL, vcfType.name(), caller.toUpperCase()),
+        return new BaseDataReport(patientId,
+                        String.format(PRECISION_CHECK_LABEL, vcfType.name(), caller.toUpperCase(Locale.ENGLISH)),
                         String.valueOf(precision));
     }
 
@@ -129,7 +131,7 @@ public class SomaticExtractor extends AbstractVCFExtractor {
                         isTotalCallersCountMoreThan(ONE));
         final double sensitivity = (double) callerSetsPerCallersCount.size() / setsPerCount.size();
         return new BaseDataReport(patientId,
-                        String.format(SENSITIVITY_CHECK_LABEL, vcfType.name(), caller.toUpperCase()),
+                        String.format(SENSITIVITY_CHECK_LABEL, vcfType.name(), caller.toUpperCase(Locale.ENGLISH)),
                         String.valueOf(sensitivity));
     }
 
