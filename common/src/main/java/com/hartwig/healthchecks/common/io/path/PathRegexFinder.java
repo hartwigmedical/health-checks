9d120205cb283a6911e5d9eb405c18d15f27b226
Noha Hazaa <nhazaa@schubergphilis.com>
2016-07-22 14:38:45 +0200

    Make sure file.walk is in try to close resources Version pushed to 1.0.3

--- common/src/main/java/com/hartwig/healthchecks/common/io/path/PathRegexFinder.java
+++ common/src/main/java/com/hartwig/healthchecks/common/io/path/PathRegexFinder.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -20,12 +21,18 @@ public interface PathRegexFinder {
     @NotNull
     static PathRegexFinder build() {
         return (path, regex) -> {
-            final Optional<Path> searchedFile = Files.walk(new File(path).toPath())
-                            .filter(filePath -> filePath.getFileName().toString().matches(regex)).findFirst();
+            final Optional<Path> searchedFile = getPath(path, regex);
             if (!searchedFile.isPresent()) {
                 throw new FileNotFoundException(String.format(FILE_S_NOT_FOUND_MSG, regex, path));
             }
             return searchedFile.get();
         };
     }
+
+    static Optional<Path> getPath(final String path, final String regex) throws IOException {
+        try (Stream<Path> paths = Files.walk(new File(path).toPath())) {
+            return paths.filter(filePath -> filePath.getFileName().toString().matches(regex)).findFirst();
+        }
+    }
+
 }
