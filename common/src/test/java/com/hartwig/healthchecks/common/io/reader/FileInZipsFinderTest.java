3bfcd9e9cfd550ad130ae30853de6f63664fd0a0
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2016-09-06 16:46:23 +0200

    Simplified tests for files in zip finder

--- common/src/test/java/com/hartwig/healthchecks/common/io/reader/FileInZipsFinderTest.java
+++ common/src/test/java/com/hartwig/healthchecks/common/io/reader/FileInZipsFinderTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.zip.ZipEntry;
+import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
 
 import com.google.common.io.Resources;
@@ -17,26 +18,29 @@ import org.junit.Test;
 
 public class FileInZipsFinderTest {
 
-    private static final String ZIP_FILE = "CPCT12345678T_FLOWCELL_S2_L002_R2_001_fastqc.zip";
-    private static final String FASTQC_DATA_FILE_NAME = "fastqc_data.txt";
-    private static final String SAMPLE = "CPCT12345678T";
-    private static final String TEST_DIR = "160101_HMFregCPCT_FR10002000_FR20003000_CPCT12345678";
-    private static final String QC_STATS = "QCStats";
+    private static final String EMPTY_FILE = "emptyfile";
+    private static final String ZIP_WITH_EMPTY_FILE = "emptyfile.zip";
+    private static final String ZIP_WITHOUT_FILE = "emptyarchive.zip";
+    private static final String ZIP_DIRECTORY = "zipfiles";
 
     @Test
-    public void readLines() throws IOException, HealthChecksException {
-        final URL testPath = Resources.getResource(
-                TEST_DIR + File.separator + SAMPLE + File.separator + QC_STATS + File.separator + ZIP_FILE);
+    public void findFile() throws IOException, HealthChecksException {
+        final URL testPath = Resources.getResource(ZIP_DIRECTORY + File.separator + ZIP_WITH_EMPTY_FILE);
         final ZipFile zipFile = new ZipFile(testPath.getPath());
-        final List<? extends ZipEntry> zipEntry = FileInZipsFinder.build().findFileInZip(zipFile,
-                        FASTQC_DATA_FILE_NAME);
-        assertNotNull("zip entry is null", zipEntry);
+        final List<? extends ZipEntry> zipEntry = FileInZipsFinder.build().findFileInZip(zipFile, EMPTY_FILE);
+        assertNotNull(zipEntry);
+    }
+
+    @Test(expected = ZipException.class)
+    public void readZipNotFound() throws IOException, HealthChecksException {
+        final URL testPath = Resources.getResource(ZIP_DIRECTORY + File.separator + ZIP_WITHOUT_FILE);
+        final ZipFile zipFile = new ZipFile(testPath.getPath());
+        FileInZipsFinder.build().findFileInZip(zipFile, "bla");
     }
 
     @Test(expected = FileNotFoundException.class)
     public void readFileNotFound() throws IOException, HealthChecksException {
-        final URL testPath = Resources.getResource(
-                TEST_DIR + File.separator + SAMPLE + File.separator + QC_STATS + File.separator + ZIP_FILE);
+        final URL testPath = Resources.getResource(ZIP_DIRECTORY + File.separator + ZIP_WITH_EMPTY_FILE);
         final ZipFile zipFile = new ZipFile(testPath.getPath());
         FileInZipsFinder.build().findFileInZip(zipFile, "bla");
     }
