edf6c666cae270a69551c64794a4c8d00fd3b79c
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2016-09-13 17:02:59 +0200

    Remove obsolete class

--- common/src/main/java/com/hartwig/healthchecks/common/checks/HealthCheckRunner.java
+++ common/src/main/java/com/hartwig/healthchecks/common/checks/HealthCheckRunner.java
@@ -1,18 +1,32 @@
 package com.hartwig.healthchecks.common.checks;
 
+import java.io.IOException;
+
+import com.hartwig.healthchecks.common.exception.HealthChecksException;
 import com.hartwig.healthchecks.common.io.dir.RunContext;
 import com.hartwig.healthchecks.common.result.BaseResult;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
 public final class HealthCheckRunner {
+
+    private static final Logger LOGGER = LogManager.getLogger(HealthCheckRunner.class);
+    private static final String ERROR_MSG = "Got an exception with message: %s";
 
     private HealthCheckRunner() {
     }
 
     @NotNull
     public static BaseResult run(@NotNull final RunContext runContext, @NotNull final HealthChecker checker) {
-        final ErrorHandlingChecker errorHandlingChecker = new ErrorHandlingChecker(checker);
-        return errorHandlingChecker.checkedRun(runContext);
+        BaseResult result;
+        try {
+            result = checker.run(runContext);
+        } catch (IOException | HealthChecksException exception) {
+            LOGGER.error(String.format(ERROR_MSG, exception.getMessage()));
+            result = checker.errorResult(runContext);
+        }
+        return result;
     }
 }
