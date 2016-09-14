db05d6c9f81d9e8881e981baf1e69b2225e01bf6
Korneel Duyvesteyn <korneel.duyvesteyn@gmail.com>
2016-09-14 12:14:41 +0200

    Refactored

--- common/src/test/java/com/hartwig/healthchecks/common/io/path/PathsExtensionFinderTest.java
+++ common/src/test/java/com/hartwig/healthchecks/common/io/path/PathsExtensionFinderTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.util.List;
 
@@ -13,19 +14,21 @@ import org.junit.Test;
 
 public class PathsExtensionFinderTest {
 
-    private static final String TEST_DIRECTORY =  Resources.getResource("zipFiles").getPath();
+    private static final String RUN_DIRECTORY = "zipFiles";
 
     private static final String EXISTING_EXTENSION = ".zip";
     private static final String NON_EXISTING_EXTENSION = ".bla";
 
     @Test
     public void findPaths() throws IOException {
-        final List<Path> paths = PathsExtensionFinder.build().findPaths(TEST_DIRECTORY, EXISTING_EXTENSION);
+        final URL testPath = Resources.getResource(RUN_DIRECTORY);
+        final List<Path> paths = PathsExtensionFinder.build().findPaths(testPath.getPath(), EXISTING_EXTENSION);
         assertTrue(!paths.isEmpty());
     }
 
     @Test(expected = FileNotFoundException.class)
     public void findPathsFilesNotFound() throws IOException {
-        PathsExtensionFinder.build().findPaths(TEST_DIRECTORY, NON_EXISTING_EXTENSION);
+        final URL testPath = Resources.getResource(RUN_DIRECTORY);
+        PathsExtensionFinder.build().findPaths(testPath.getPath(), NON_EXISTING_EXTENSION);
     }
 }
