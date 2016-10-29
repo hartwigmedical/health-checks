a784bc62cf0e2234acfdb943059e8b5cba989f97
Sam Brightman <sam.brightman@gmail.com>
2016-10-29 06:44:28 +0200

    Make date parsing more flexible

--- waternoose/src/main/java/com/hartwig/healthchecks/waternoose/check/MetadataChecker.java
+++ waternoose/src/main/java/com/hartwig/healthchecks/waternoose/check/MetadataChecker.java
@@ -9,6 +9,11 @@ import java.util.Locale;
 import java.util.function.Predicate;
 
 import com.google.common.collect.Lists;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
+
 import com.hartwig.healthchecks.common.checks.CheckType;
 import com.hartwig.healthchecks.common.checks.ErrorHandlingChecker;
 import com.hartwig.healthchecks.common.checks.HealthCheck;
@@ -22,10 +27,6 @@ import com.hartwig.healthchecks.common.resource.ResourceWrapper;
 import com.hartwig.healthchecks.common.result.BaseResult;
 import com.hartwig.healthchecks.common.result.PatientResult;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.jetbrains.annotations.NotNull;
-
 @SuppressWarnings("WeakerAccess")
 @ResourceWrapper(type = CheckType.METADATA)
 public class MetadataChecker extends ErrorHandlingChecker implements HealthChecker {
@@ -34,7 +35,10 @@ public class MetadataChecker extends ErrorHandlingChecker implements HealthCheck
 
     private static final String LOG_FILENAME_FORMAT = "%s.log";
     private static final String DATE_OUT_FORMAT = "yyyy-MM-dd";
-    private static final String DATE_IN_FORMAT = "EEE MMM d HH:mm:ss z yyyy";
+    private static final String DATE_IN_FORMAT = "[EEE MMM d HH:mm:ss z yyyy]"
+                                                 + "[EEE d MMM HH:mm:ss z yyyy]"
+                                                 + "[EEE d MMM yyyy HH:mm:ss z]"
+                                                 + "[EEE MMM d yyyy HH:mm:ss z]";
     private static final String REGEX_SPLIT = "\t";
     private static final String LINE_TO_GET_DATE_FROM = "End Kinship";
     private static final int DATE_LINE_INDEX = 0;
@@ -55,7 +59,8 @@ public class MetadataChecker extends ErrorHandlingChecker implements HealthCheck
 
     @NotNull
     @Override
-    public BaseResult tryRun(@NotNull final RunContext runContext) throws IOException, HealthChecksException {
+    public BaseResult tryRun(@NotNull final RunContext runContext)
+            throws IOException, HealthChecksException {
         final String runDate = extractRunDate(runContext);
         final String pipelineVersion = extractPipelineVersion(runContext.runDirectory());
 
@@ -77,12 +82,17 @@ public class MetadataChecker extends ErrorHandlingChecker implements HealthCheck
     }
 
     @NotNull
-    private BaseResult toPatientResult(@NotNull final RunContext runContext, @NotNull final String runDate,
-            @NotNull final String pipelineVersion) {
-        final List<HealthCheck> refMetaData = toHealthCheckList(runContext.refSample(), runContext, runDate,
-                pipelineVersion);
-        final List<HealthCheck> tumorMetaData = toHealthCheckList(runContext.tumorSample(), runContext, runDate,
-                pipelineVersion);
+    private BaseResult toPatientResult(@NotNull final RunContext runContext,
+                                       @NotNull final String runDate,
+                                       @NotNull final String pipelineVersion) {
+        final List<HealthCheck> refMetaData = toHealthCheckList(runContext.refSample(),
+                                                                runContext,
+                                                                runDate,
+                                                                pipelineVersion);
+        final List<HealthCheck> tumorMetaData = toHealthCheckList(runContext.tumorSample(),
+                                                                  runContext,
+                                                                  runDate,
+                                                                  pipelineVersion);
 
         HealthCheck.log(LOGGER, refMetaData);
         HealthCheck.log(LOGGER, tumorMetaData);
@@ -92,23 +102,29 @@ public class MetadataChecker extends ErrorHandlingChecker implements HealthCheck
 
     @NotNull
     private static List<HealthCheck> toHealthCheckList(@NotNull final String sampleId,
-            @NotNull final RunContext runContext, @NotNull final String runDate,
-            @NotNull final String pipelineVersion) {
+                                                       @NotNull final RunContext runContext,
+                                                       @NotNull final String runDate,
+                                                       @NotNull final String pipelineVersion) {
         return Lists.newArrayList(new HealthCheck(sampleId, MetadataCheck.RUN_NAME.toString(), runContext.runName()),
-                new HealthCheck(sampleId, MetadataCheck.HAS_PASSED_TESTS.toString(),
-                        Boolean.toString(runContext.hasPassedTests())),
-                new HealthCheck(sampleId, MetadataCheck.RUN_DATE.toString(), runDate),
-                new HealthCheck(sampleId, MetadataCheck.PIPELINE_VERSION.toString(), pipelineVersion));
+                                  new HealthCheck(sampleId,
+                                                  MetadataCheck.HAS_PASSED_TESTS.toString(),
+                                                  Boolean.toString(runContext.hasPassedTests())),
+                                  new HealthCheck(sampleId, MetadataCheck.RUN_DATE.toString(), runDate),
+                                  new HealthCheck(sampleId,
+                                                  MetadataCheck.PIPELINE_VERSION.toString(),
+                                                  pipelineVersion));
     }
 
     @NotNull
     private static String extractRunDate(@NotNull final RunContext runContext)
             throws IOException, HealthChecksException {
-        final Path dateTimeLogPath = PathRegexFinder.build().findPath(runContext.runDirectory(),
-                String.format(LOG_FILENAME_FORMAT, runContext.runName()));
-        final List<String> dateLines = LineReader.build().readLines(dateTimeLogPath,
-                doesLineStartWith(LINE_TO_GET_DATE_FROM));
-        final String date = dateLines.get(DATE_LINE_INDEX).split(REGEX_SPLIT)[1].trim();
+        final Path dateTimeLogPath = PathRegexFinder.build()
+                                                    .findPath(runContext.runDirectory(),
+                                                              String.format(LOG_FILENAME_FORMAT,
+                                                                            runContext.runName()));
+        final List<String> dateLine = LineReader.build()
+                                                .readLines(dateTimeLogPath, doesLineStartWith(LINE_TO_GET_DATE_FROM));
+        final String date = dateLine.get(DATE_LINE_INDEX).split(REGEX_SPLIT)[1].trim();
         final DateTimeFormatter inFormatter = DateTimeFormatter.ofPattern(DATE_IN_FORMAT, Locale.ENGLISH);
         final LocalDateTime formattedDate = LocalDateTime.parse(date, inFormatter);
         final DateTimeFormatter outFormatter = DateTimeFormatter.ofPattern(DATE_OUT_FORMAT, Locale.ENGLISH);
@@ -118,10 +134,10 @@ public class MetadataChecker extends ErrorHandlingChecker implements HealthCheck
     @NotNull
     private static String extractPipelineVersion(@NotNull final String runDirectory)
             throws IOException, HealthChecksException {
-        final Path pipelineLog = PathRegexFinder.build().findPath(runDirectory, PIPELINE_LOG_REGEX);
-        final List<String> versionsLines = LineReader.build().readLines(pipelineLog,
-                doesLineStartWith(PIPELINE_VERSION));
-        return versionsLines.get(PIPELINE_VERSION_LINE_INDEX).split(PIPELINE_VERSION_LINE_SEPARATOR)[1].trim();
+        final Path pipelineLogPath = PathRegexFinder.build().findPath(runDirectory, PIPELINE_LOG_REGEX);
+        final List<String> versionsLine = LineReader.build()
+                                                    .readLines(pipelineLogPath, doesLineStartWith(PIPELINE_VERSION));
+        return versionsLine.get(PIPELINE_VERSION_LINE_INDEX).split(PIPELINE_VERSION_LINE_SEPARATOR)[1].trim();
     }
 
     @NotNull
